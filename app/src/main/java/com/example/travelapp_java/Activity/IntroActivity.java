4924ccc5d1626ad3882cbed0f83e22ebbc45e21d
Muhammet Şevki UZUN <sevki_uzun53@hotmail.com>
2024-06-23 22:24:45 +0300

    Location  & banners & category backend

--- app/src/main/java/com/example/travelapp_java/Activity/IntroActivity.java
+++ app/src/main/java/com/example/travelapp_java/Activity/IntroActivity.java
@@ -1,14 +1,7 @@
-package com.example.travelapp_java;
+package com.example.travelapp_java.Activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.example.travelapp_java.databinding.ActivityIntroBinding;
 
---
+++ app/src/main/java/com/example/travelapp_java/Domain/SliderItems.java
@@ -0,0 +1,17 @@
+package com.example.travelapp_java.Domain;
+
+public class SliderItems {
+    private String url;
+
+    public SliderItems() {
+
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
