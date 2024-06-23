535f64962197bf8ac5f2a737d7b4769273f9ffe3
Muhammet Şevki UZUN <sevki_uzun53@hotmail.com>
2024-06-23 17:39:08 +0300

    Intro Activity

---
+++ app/src/main/java/com/example/travelapp_java/IntroActivity.java
@@ -0,0 +1,25 @@
+package com.example.travelapp_java;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.travelapp_java.databinding.ActivityIntroBinding;
+
+public class IntroActivity extends BaseActivity {
+    ActivityIntroBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityIntroBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.introBtn.setOnClickListener(v -> startActivity(new Intent(IntroActivity.this,MainActivity.class)));
+    }
+}
